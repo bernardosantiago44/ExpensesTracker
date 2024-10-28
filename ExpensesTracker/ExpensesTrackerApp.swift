eab063fd624400be09bb2e9002ffb683962124ff
Bernardo Santiago <bernardosantiago44@gmail.com>
2024-10-28 10:32:45 -0600

    Initial Commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Bernardo Santiago Marin on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpensesTrackerTests/ExpensesTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerTests.swift
+//  ExpensesTrackerTests
+//
+//  Created by Bernardo Santiago Marin on 28/10/24.
+//
+
+import Testing
+@testable import ExpensesTracker
+
+struct ExpensesTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
