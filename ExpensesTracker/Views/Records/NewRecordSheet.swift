0da7cdedd4fef3c010dc425bcfd41edd377a539d
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2024-11-20 18:44:10 -0600

    File for new record sheet.

---
+++ ExpensesTracker/Views/Records/NewRecordSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NewRecordSheet.swift
+//  ExpensesTracker
+//
+//  Created by Bernardo Santiago Marin on 20/11/24.
+//
+
+import SwiftUI
+
+struct NewRecordSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewRecordSheet()
+}
